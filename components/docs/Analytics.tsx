684b8d76e85e6d17f24c2467ef1b1bf918050395
Denish Navadiya <denishnavadiya1@gmail.com>
2025-06-25 11:36:39 +0530

    feat: improve analytics

--- components/docs/Analytics.tsx
+++ components/docs/Analytics.tsx
@@ -6,7 +6,7 @@ export const Analytics = () => {
     return (
         <VercelAnalytics
             beforeSend={(event: BeforeSendEvent) => {
-                if (event.type === "pageview" && event.url === "/") {
+                if (event.type === "pageview" && event.url === "https://www.paceui.com/") {
                     return event;
                 }
                 return null;
